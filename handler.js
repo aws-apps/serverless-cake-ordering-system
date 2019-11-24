4eb59504e55afd7080b34a1b53576476f8552974
Pradeep Kumar <pradeepk.pdp@gmail.com>
2019-11-24 20:27:37 +0530

    feat(create-order): add create order endpoint

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
-'use strict';
-
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+module.exports = {
+  createOrder: async(event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hey there, Hello from cake-ordering-system, we have received your order!!',
         input: event,
       },
       null,
       2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+      ),
+    }
+  }
 };
